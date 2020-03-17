1aec95d3de9a5d8c2ad2214ea6298a74a32832d2
Khoa Bui <kbui@zendesk.com>
2020-03-17 12:26:54 +0000

    Add edge case check

--- src/services/greeting/quote.js
+++ src/services/greeting/quote.js
@@ -1,6 +1,10 @@
 const fetch = require('node-fetch')
 
 module.exports = async () => {
+  if (typeof process.env.QUOTE_URL !== 'string') {
+    throw new Error('Missing QUOTE_URL in .env')
+  }
+
   const response = await fetch(process.env.QUOTE_URL)
   const json = await response.json()
 

8895a3779d285dfebf8647f992dc3efe2ae52a93
Khoa Bui <kbui@zendesk.com>
2020-03-22 18:20:30 +0000

    Transform callback to promise for memjs function

--- src/services/cache/memcached.js
+++ src/services/cache/memcached.js
@@ -1,13 +1,17 @@
 const { Client } = require('memjs')
+const util = require('util')
 
 const client = Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
   username: process.env.MEMCACHEDCLOUD_USERNAME,
   password: process.env.MEMCACHEDCLOUD_PASSWORD,
 })
 
+const clientSetCache = util.promisify(client.set)
+const clientGetCache = util.promisify(client.get)
+
 const setCache = async (key, data, ttl) => {
   try {
-    await client.set(key, data, { expires: ttl })
+    await clientSetCache(key, data, { expires: ttl })
     return data
   } catch (err) {
     return undefined
@@ -16,7 +20,7 @@ const setCache = async (key, data, ttl) => {
 
 const getCache = async (key) => {
   try {
-    const data = await client.get(key)
+    const data = await clientGetCache(key)
     return data
   } catch (err) {
     return undefined

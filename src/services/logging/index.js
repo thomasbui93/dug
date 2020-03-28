4f2841dc2549544160502d1a1c1b9357b265283c
Khoa Bui <kbui@zendesk.com>
2020-03-28 12:58:00 +0000

    Configure log file in dev env

--- src/services/logging/index.js
+++ src/services/logging/index.js
@@ -1,3 +1,3 @@
 const pino = require('pino')
 
-module.exports = process.env.NODE_ENV === 'production' ? require('./sentry') : pino(pino.destination('./log'))
+module.exports = process.env.NODE_ENV === 'production' ? require('./sentry') : pino(pino.destination('./.log'))

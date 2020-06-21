0678b307288e799fdea5dbf7fa73c18bb7e02b5a
Khoa Bui <kbui@zendesk.com>
2020-06-22 00:13:09 +0100

    Add internal auth method to admin endpoints

--- src/web/index.js
+++ src/web/index.js
@@ -2,11 +2,12 @@ const healthCheck = require('./health_check')
 const greeting = require('./api/greeting')
 const poem = require('./api/poem')
 const admin = require('./admin')
+const internalAuth = require('../middleware/auth/internal')
 
 module.exports = (app) => {
   app.use('/z/check', healthCheck)
   app.use('/api/greeting', greeting)
   app.use('/api/poem', poem)
 
-  app.use('/admin', admin)
+  app.use('/admin', internalAuth, admin)
 }
